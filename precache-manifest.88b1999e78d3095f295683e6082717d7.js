f709b1fafa3b195927fc3c2fed99d6d1ab452c88
Kerim\Kerim <k-kubilay@hotmail.com>
2019-09-01 22:03:43 +0300

    Updates

---
+++ precache-manifest.88b1999e78d3095f295683e6082717d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2307a7b3a036cf3de2b432e58c9cb75b",
+    "url": "/tobb/index.html"
+  },
+  {
+    "revision": "221af5211e73d390627e",
+    "url": "/tobb/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "53c70ee95c787c9b2fe3",
+    "url": "/tobb/static/js/2.57534d04.chunk.js"
+  },
+  {
+    "revision": "221af5211e73d390627e",
+    "url": "/tobb/static/js/main.e7004a0d.chunk.js"
+  },
+  {
+    "revision": "47d0be261de994cf466c",
+    "url": "/tobb/static/js/runtime~main.fcbdaca6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tobb/static/media/logo.5d5d9eef.svg"
+  }
+]);
