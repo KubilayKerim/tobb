0549b8b0cf909963ecdabb608acf6ecbe23074b3
Kerim\Kerim <k-kubilay@hotmail.com>
2019-09-03 23:16:07 +0300

    rerearrenged build

---
+++ tobb-react/build/precache-manifest.eef5734e9f88ab92c77143a3afdf09c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb94f8f525a4220f4c07cbcf6d50db2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54a3bce172a3c2ed3cd2",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "53c70ee95c787c9b2fe3",
+    "url": "./static/js/2.57534d04.chunk.js"
+  },
+  {
+    "revision": "54a3bce172a3c2ed3cd2",
+    "url": "./static/js/main.6ad01254.chunk.js"
+  },
+  {
+    "revision": "96e4a48cf3ad9ddcd7f3",
+    "url": "./static/js/runtime~main.939080f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- tobb-react/build/service-worker.js
+++ tobb-react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tobb/tobb-react/precache-manifest.4c436fa6fb3e866dd7372eecbe888256.js"
+  "./precache-manifest.eef5734e9f88ab92c77143a3afdf09c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tobb/tobb-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
